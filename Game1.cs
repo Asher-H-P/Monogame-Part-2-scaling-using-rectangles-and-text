aeac491ab1bfeb95bd637ab8f65e54355c491ed8
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-11-28 11:00:18 -0500

    Did some.

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,8 @@ namespace Monogame_Part_2_scaling_using_rectangles_and_text
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D circleTexture;
+        Texture2D rectangleTexture;
 
         public Game1()
         {
@@ -19,14 +21,17 @@ namespace Monogame_Part_2_scaling_using_rectangles_and_text
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            circleTexture = Content.Load<Texture2D>("circle");
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
             // TODO: use this.Content to load your game content here
         }
 
@@ -42,9 +47,19 @@ namespace Monogame_Part_2_scaling_using_rectangles_and_text
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Peru);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(circleTexture, new Rectangle(125, 30, 120, 120), Color.Pink);
+            _spriteBatch.Draw(circleTexture, new Rectangle(550, 30, 120, 120), Color.Pink);
+            _spriteBatch.Draw(circleTexture, new Rectangle(145, 50, 90, 90), Color.HotPink);
+            _spriteBatch.Draw(circleTexture, new Rectangle(565, 50, 90, 90), Color.HotPink);
+            _spriteBatch.Draw(circleTexture, new Rectangle(100, 50, 600, 400), Color.Pink);
+            _spriteBatch.Draw(circleTexture, new Rectangle(460, 140, 125, 75), Color.White);
+            _spriteBatch.Draw(circleTexture, new Rectangle(220, 140, 125, 75), Color.White);
+            
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
